cba6d12568631261870deae0efd87e2639434a64
Yy <73810807+akumakumu@users.noreply.github.com>
2022-12-22 23:17:59 +0700

    old adguardhome that i didnt maintenance anymore

---
+++ adguardhome.sh
@@ -0,0 +1,10 @@
+docker run --name adguardhome\
+    --restart unless-stopped\
+    -v /adguardhome/work:/opt/adguardhome/work\
+    -v /adguardhome/conf:/opt/adguardhome/conf\
+    -p 53:53/tcp -p 53:53/udp\
+    -p 80:80/tcp -p 443:443/tcp -p 443:443/udp -p 3000:3000/tcp\
+    -p 853:853/tcp\
+    -p 784:784/udp -p 853:853/udp -p 8853:8853/udp\
+    -p 5443:5443/tcp -p 5443:5443/udp\
+    -d adguard/adguardhome
