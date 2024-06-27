df64b2dbc826a6777ca11aece9362aefa5a6e011
Yy <akumakumu@gmail.com>
2024-06-28 03:15:04 +0700

    fixing redis stack mount

--- postgresql.sh
+++ postgresql.sh
@@ -1,6 +1,6 @@
-sudo docker run\ 
-    --name postgres-01\ 
-    -d \ 
-    -p 5432:5432\ 
-    -e POSTGRES_PASSWORD=yourpassword\ 
+docker run \
+    --name postgres-01 \
+    -d \
+    -p 5432:5432 \
+    -e POSTGRES_PASSWORD=yourpassword \
     postgres
